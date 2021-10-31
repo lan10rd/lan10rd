d982b4b58bed0b5ef77f3fa04883325f8521c3a6
lanl0rd <k1ng@glass.earth>
2021-10-31 15:08:29 -0400

    laugh

---
+++ grams/apps/host-api-ns/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'host-api-ns',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/host-api-ns',
+};
