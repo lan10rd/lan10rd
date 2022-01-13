6b7b58dab6b8f022b90a982100ee58f4f005ce9a
simba <wakeboarder930@gmail.com>
2022-01-13 14:00:06 -0500

    adding pwa to reference app

--- grams/apps/reference-app-ng/src/app/app.module.ts
+++ grams/apps/reference-app-ng/src/app/app.module.ts
@@ -26,7 +26,13 @@ import { ScullyLibModule } from '@scullyio/ng-lib'
             enabled: environment.production,
             registrationStrategy: 'registerWhenStable:30000'
         }),
-        ScullyLibModule
+        ScullyLibModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+          enabled: environment.production,
+          // Register the ServiceWorker as soon as the app is stable
+          // or after 30 seconds (whichever comes first).
+          registrationStrategy: 'registerWhenStable:30000'
+        })
     ],
     providers: 
     [
