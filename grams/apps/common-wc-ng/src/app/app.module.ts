bfde8bf5d9376d6002f18758ba59eaa500ae46dd
simba <wakeboarder930@gmail.com>
2022-01-24 15:14:08 -0500

    publishing github

--- grams/apps/common-wc-ng/src/app/app.module.ts
+++ grams/apps/common-wc-ng/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule } from '@angular/common/http'
 
 import { 
   CommonNgSelectElement, CommonNgSelectElementModule,
-  CommonNgButtonElement, CommonNgButtonElementModule
+  CommonNgButtonElement, CommonNgButtonElementModule,
+  CommonNgPopopElement, CommonNgPopopElementModule,
+  CommonNgThemesElement, CommonNgThemesElementModule,
 } from '@grams/common/ng'
 
 @NgModule
 ({
-  imports: [BrowserModule, HttpClientModule, CommonNgSelectElementModule],
+  imports: [BrowserModule, HttpClientModule, CommonNgSelectElementModule, CommonNgButtonElementModule, CommonNgPopopElementModule, CommonNgThemesElementModule, ],
   declarations: [],
   providers: [],
   bootstrap: [],
@@ -19,13 +21,17 @@ import {
 export class AppModule {
   constructor
   (
-      injector: Injector
+    injector: Injector
   )
   {
     const customSelect = createCustomElement(CommonNgSelectElement, {injector: injector})
     customElements.get('common-ng-select-element-wc') || customElements.define('common-ng-select-element-wc', customSelect)
     const customButton = createCustomElement(CommonNgButtonElement, {injector: injector})
     customElements.get('common-ng-button-element-wc') || customElements.define('common-ng-button-element-wc', customButton)
+    const customPopop = createCustomElement(CommonNgPopopElement, {injector: injector})
+    customElements.get('common-ng-popop-element-wc') || customElements.define('common-ng-popop-element-wc', customPopop)
+    const customThemes = createCustomElement(CommonNgThemesElement, {injector: injector})
+    customElements.get('common-ng-themes-element-wc') || customElements.define('common-ng-themes-element-wc', customThemes)
   }
 
   ngDoBootstrap() {}
--- grams/etc/run/wc/common-wc-ng.sh
+++ grams/etc/run/wc/common-wc-ng.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
-nx build common-wc-ng --prod --output-hashing=none
-# where is scripts.js before main.js coming from ~/lan10rd/grams/dist/apps/common-wc-ng/scripts.js
-cat ~/lan10rd/grams/dist/apps/common-wc-ng/runtime.js ~/lan10rd/grams/dist/apps/common-wc-ng/polyfills.js  ~/lan10rd/grams/dist/apps/common-wc-ng/main.js > ~/lan10rd/grams/dist/apps/common-wc-ng/common-wc-ng.js
+# #!/bin/sh
+# nx build common-wc-ng --prod --output-hashing=none
+# # where is scripts.js before main.js coming from ~/lan10rd/grams/dist/apps/common-wc-ng/scripts.js
+# cat ~/lan10rd/grams/dist/apps/common-wc-ng/runtime.js ~/lan10rd/grams/dist/apps/common-wc-ng/polyfills.js  ~/lan10rd/grams/dist/apps/common-wc-ng/main.js > ~/lan10rd/grams/dist/apps/common-wc-ng/common-wc-ng.js
+
+nx build common-wc-ng
