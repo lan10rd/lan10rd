5aa4d178e008144fed81bc8f870909328162d458
simba <wakeboarder930@gmail.com>
2022-01-31 10:15:14 -0500

    rekt

---
+++ grams/apps/reference-api-ja/src/main/java/grams/referenceapija/ReferenceApiJaApplication.java
@@ -0,0 +1,13 @@
+package grams.referenceapija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferenceApiJaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferenceApiJaApplication.class, args);
+	}
+
+}
---
+++ grams/apps/reference-api-ja/src/test/java/grams/referenceapija/ReferenceApiJaApplicationTests.java
@@ -0,0 +1,13 @@
+package grams.referenceapija;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReferenceApiJaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
