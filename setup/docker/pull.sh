a5cfb5e2d408fdcae1b9825b7c1f5340f81383fc
Nik Caryl <nikcaryl@Niks-MacBook-Air.local>
2023-03-21 12:47:47 -0700

    ok

---
+++ setup/docker/pull.sh
@@ -0,0 +1,52 @@
+docker pull ubuntu
+docker pull alpine
+
+docker pull nginx
+docker pull jc21/nginx-proxy-manager
+docker pull traefik
+
+docker pull oznu/guacamole
+docker pull snowdreamtech/frps
+docker pull snowdreamtech/frpc
+docker pull certbot/certbot
+
+docker pull node
+docker pull node:18
+docker pull node:17
+docker pull node:16
+docker pull node:15
+docker pull node:14
+docker pull node:13
+docker pull node:12
+
+docker pull openjdk
+docker pull openjdk:21
+docker pull openjdk:20
+docker pull openjdk:19
+docker pull openjdk:18
+docker pull openjdk:17
+docker pull openjdk:16
+docker pull openjdk:11
+docker pull openjdk:8s
+
+docker pull golang
+docker pull python
+docker pull pypy
+
+docker pull postgresql
+docker pull mysql
+docker pull mariadb
+docker pull mongo
+docker pull mongo-express
+docker pull redis
+
+docker pull elasticsearch
+docker pull kibana
+docker pull wordpress
+
+docker pull httpd
+docker pull memcached
+docker pull rabbitmq
+
+docker pull datadog/agent
+docker pull busybox
