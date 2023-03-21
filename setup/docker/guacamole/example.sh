ae818161945df0341b53faec4d075dd53dbe8f45
simba <simba@simba.local>
2023-03-21 15:41:21 -0700

    ok

--- setup/docker/guacamole/example.sh
+++ setup/docker/guacamole/example.sh
@@ -2,9 +2,9 @@ docker run \
   -dit \
   -p 8080:8080 \
   --name lan10rd_guacamole \
-  -v ~/lan10rd/setup/guacamole/config:/config \
+  -v ~/lan10rd/setup/docker/guacamole/config:/config \
   --restart=always \
-  lan10rd/guacamole
+  oznu/guacamole
 
 #   -e "EXTENSIONS=auth-ldap,auth-duo"
 # maxwaldorf/guacamoleclear
