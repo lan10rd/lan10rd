e7828ca15a655661372e5c9fbac77c67ec63c36a
simba <simba@simba.local>
2023-07-08 17:45:21 -0700

    ok

--- setup/docker/stacks/run-swarm-host.sh
+++ setup/docker/stacks/run-swarm-host.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+
 # run this after you have a domain name, then on a dedicated ip provider run frps (instructions in frp folder setup-docker-frps.sh)  dont forget to add the static ip to your frpc.ini file, but thats it...i like digital ocean spin up a droplet, open console, pull lan10rd, and run setup docker frps, then run frps.yaml 
+cd "$(dirname "$0")
 
 docker stack deploy --prune -c ./frpc.yaml lan10rd
 docker stack deploy --prune -c ./guacamole.yaml lan10rd
