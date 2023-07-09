e7da2a0f85cb11466564acb1e6ca6802b281516e
simba <simba@simba.local>
2023-07-08 19:45:43 -0700

    ok

--- setup/docker/host.sh
+++ setup/docker/host.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# run this after you have a domain name, then on a dedicated ip provider run frps (instructions in frp folder setup-docker-frps.sh)  dont forget to add the static ip to your frpc.ini file, but thats it...i like digital ocean spin up a droplet, open console, clone lan10rd, and run setup docker frps, then run frps.yaml 
-# setup 2 A records, one with * and one with @ at the ip address your frps is running on
-cd $(dirname "$0")
+# this setup assumes you are running from a non static ip address and this is your first time running at a new site, this will give you the ability to start websites/webservices and connect to client machines on the same lan.
 
-# docker stack deploy --prune -c ./frpc.yaml lan10rd
-# docker stack deploy --prune -c ./guacamole.yaml lan10rd
-# docker stack deploy --prune -c ./nginx-proxy-manager.yaml lan10rd
+# so for this to work youll need (1) a domain name (2) a dedicated ip from a service like digital ocean, ie spin up a new droplet, login to that console and git clone lan10rd and run setup-docker-frps.sh script inside setup/docker/frp
+
+# it's recommended to copy setup/docker folder into somewhere like lan10rd/space to keep a clean reference.
+
+# the only configuration change needed is to insert the ip address from your frps instance (you'll also want to add 2 A records (@ and *) in your dns setting)!
+
+cd $(dirname "$0")
 
 docker-compose -d -f host.yaml up
