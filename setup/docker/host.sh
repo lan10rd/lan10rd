81db512185b8a1e4a09a0520030d96f914c71421
simba <simba@simba.local>
2023-07-08 19:39:01 -0700

    fixes

--- setup/docker/host.sh
+++ setup/docker/host.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # run this after you have a domain name, then on a dedicated ip provider run frps (instructions in frp folder setup-docker-frps.sh)  dont forget to add the static ip to your frpc.ini file, but thats it...i like digital ocean spin up a droplet, open console, clone lan10rd, and run setup docker frps, then run frps.yaml 
+# setup 2 A records, one with * and one with @ at the ip address your frps is running on
 cd $(dirname "$0")
 
 # docker stack deploy --prune -c ./frpc.yaml lan10rd
 # docker stack deploy --prune -c ./guacamole.yaml lan10rd
 # docker stack deploy --prune -c ./nginx-proxy-manager.yaml lan10rd
 
-docker-compose -f host.yaml up
+docker-compose -d -f host.yaml up
